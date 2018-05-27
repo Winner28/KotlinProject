8b7a21d4ce7a431a194ae88831aa59a41b561ce9
vlaDey <bagdeyv@gmail.com>
2018-05-27 22:53:27 +0300

    Initial commit

---
+++ backend/src/main/kotlin/MainKt.kt
@@ -0,0 +1,10 @@
+import io.ktor.application.*
+import io.ktor.http.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+
+fun main(args: Array<String>) {
+
+}
